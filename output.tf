d160f7971848971d1ae365394bd3948e4a53aef0
capybara1 <39344016+capybara1@users.noreply.github.com>
2020-04-15 19:04:15 +0200

    feat: providing availability zone names

--- output.tf
+++ output.tf
@@ -5,10 +5,18 @@ output "vpc_id" {
 
 output "public_subnets" {
   description = "The ids of the created public subnets"
-  value       = [for i in range(var.number_of_public_subnets) : { id = aws_subnet.public[i].id, availability_zone_id = data.aws_availability_zones.available.zone_ids[i] }]
+  value       = [for i in range(var.number_of_public_subnets) : {
+    id = aws_subnet.public[i].id,
+    availability_zone_id = data.aws_availability_zones.available.zone_ids[i],
+    availability_zone_name = data.aws_availability_zones.available.names[i]
+  }]
 }
 
 output "private_subnets" {
   description = "The ids of the created private subnets"
-  value       = [for i in range(var.number_of_private_subnets) : { id = aws_subnet.private[i].id, availability_zone_id = data.aws_availability_zones.available.zone_ids[i] }]
+  value       = [for i in range(var.number_of_private_subnets) : {
+    id = aws_subnet.private[i].id,
+    availability_zone_id = data.aws_availability_zones.available.zone_ids[i],
+    availability_zone_name = data.aws_availability_zones.available.names[i]
+  }]
 }
--- version.tf
+++ version.tf
@@ -1,5 +1,7 @@
 terraform {
+  required_version = ">= 0.12.0"
   required_providers {
-    aws = ">= 2.7.0"
+    aws      = ">= 2.7.0"
+    template = ">= 2.1"
   }
 }
