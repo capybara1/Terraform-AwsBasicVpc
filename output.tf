100823fbc2e434b2056f0664479ea54c3bd01d68
capybara1 <39344016+capybara1@users.noreply.github.com>
2020-04-22 17:46:53 +0200

    Reformatting

--- output.tf
+++ output.tf
@@ -1,22 +1,22 @@
 output "vpc_id" {
   description = "The VPC id"
-  value = aws_vpc.default.id
+  value       = aws_vpc.default.id
 }
 
 output "public_subnets" {
   description = "The ids of the created public subnets"
-  value       = [for i in range(var.number_of_public_subnets) : {
-    id = aws_subnet.public[i].id,
-    availability_zone_id = data.aws_availability_zones.available.zone_ids[i],
+  value = [for i in range(var.number_of_public_subnets) : {
+    id                     = aws_subnet.public[i].id,
+    availability_zone_id   = data.aws_availability_zones.available.zone_ids[i],
     availability_zone_name = data.aws_availability_zones.available.names[i]
   }]
 }
 
 output "private_subnets" {
   description = "The ids of the created private subnets"
-  value       = [for i in range(var.number_of_private_subnets) : {
-    id = aws_subnet.private[i].id,
-    availability_zone_id = data.aws_availability_zones.available.zone_ids[i],
+  value = [for i in range(var.number_of_private_subnets) : {
+    id                     = aws_subnet.private[i].id,
+    availability_zone_id   = data.aws_availability_zones.available.zone_ids[i],
     availability_zone_name = data.aws_availability_zones.available.names[i]
   }]
 }
